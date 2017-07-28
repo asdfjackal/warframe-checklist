705c2d80c9a8de208518d40e5e620f589de2647b
Aleksandr <aleksandr@asdfjackal.com>
2017-07-28 18:41:39 -0400

    Updated banner text

--- src/App.js
+++ src/App.js
@@ -73,13 +73,10 @@ class App extends Component {
         <div className="App-header">
           <h2>Warframe Checklist</h2>
           <p>Last Updated For:</p>
-          <p>CHAINS OF HARROW | 2017.06.30.15.57</p>
+          <p>CHAINS OF HARROW | 2017.07.19.18.14</p>
         </div>
         <br />
         <i className="alert">Attention Tenno: The developers of this tool are taking a break from Warframe until further notice. If you notice an item is missing please contact asdfJackal on twitter or the github repository and we will add it ASAP. </i><br /><br />
-
-        <i className="alert">If you live under a rock for some reason and you have Twitch/Amazon prime make sure to get your free Frost Prime</i><br /><br />
-        <i className="alert">ALERT: Scourge was accidentally added to the wrong category. If you used the checklist since Chains of Harrow items were added, you will have to reset your local storage. I am very sorry for inconveniencing you.</i><br /><br />
 
         <i>Please Note: Clearing your Cache or Cookies will delete your progress.</i><br />
         <i>WARNING: I am still fixing bugs and data may sometimes become corrupted or lost</i><br/ >
