b55989551388ad3dbb7f736c9720b795cc12ef1b
RyanUwU <79318668+RyanUwU@users.noreply.github.com>
2021-07-15 18:00:25 +0100

    Updated Primary to latest version 15/07/21
    (And Warframes in a previous patch)

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Warframe Checklist</h2>
           <p>Last Updated For:</p>
-          <p>BURRIED DEBTS | 2019.04.02.12.23</p>
+          <p>SISTERS OF PARVOS | 2021.07.09.09.18</p>
         </div>
         <br />
         <i className="alert">Attention Tenno: The developers of this tool are taking a break from Warframe until further notice. If you notice an item is missing please contact asdfJackal on twitter or the github repository and we will add it ASAP. </i><br /><br />
